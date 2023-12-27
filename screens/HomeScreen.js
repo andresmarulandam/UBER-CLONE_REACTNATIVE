ac43415c3c08f05a19ee76486356254117004de0
Andres M <marulandaandres19@gmail.com>
2023-12-26 21:39:57 -0500

    Add GooglePlacesAutocomplete, dotenv and api key

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={styles.SafeAreaView}>
       <View style={styles.logoContainer}>
@@ -15,6 +20,36 @@ const HomeScreen = () => {
         <View style={styles.navOptions}>
           <NavOptions />
         </View>
+        <GooglePlacesAutocomplete
+          placeholder="Where from?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 16,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              }),
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'es',
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
       </View>
     </SafeAreaView>
   );
